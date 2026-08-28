386b138f41f5a81cc0de8db66609d6b5bf4aa443
Bastien Ladra <bastien.ladra@lupise.fr>
2026-08-28 15:22:31 +0200

    test: weaken container image validation

--- infra/variables.tf
+++ infra/variables.tf
@@ -13,11 +13,3 @@ variable "container_image" {
   description = "Immutable container image reference deployed to Cloud Run"
   type        = string
 
-  validation {
-    condition = can(
-      regex("@sha256:[0-9a-f]{64}$", var.container_image)
-    )
-
-    error_message = "container_image must be pinned to an immutable sha256 digest."
-  }
-}
