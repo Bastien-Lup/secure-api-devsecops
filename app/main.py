32b327f6fe02f82743c70b5bc2b2253767ca7860
Bastien Ladra <bastien.ladra@lupise.fr>
2026-08-28 11:47:54 +0200

    fix: remove unsafe dynamic code evaluation

--- app/main.py
+++ app/main.py
@@ -15,9 +15,3 @@ def read_health():
     return {
         "status": "healthy"
     }
-
-@app.get("/calculate")
-def calculate(expression: str):
-    return {
-        "result": eval(expression)
-    }
